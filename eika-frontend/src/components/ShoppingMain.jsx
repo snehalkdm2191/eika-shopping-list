2aa1fd9df4d10de777fd7fbbe1fae7b0a99418e5
snehalkdm2191 <snehalkdm2191@gmail.com>
2021-07-14 23:14:59 +0530

    Changes done for remove item from cart.

--- eika-frontend/src/components/ShoppingMain.jsx
+++ eika-frontend/src/components/ShoppingMain.jsx
@@ -13,12 +13,12 @@ function ShoppingMain() {
     if (exist) {
       setCartProduct(
         cartProduct.map((x) =>
-          x.id === product.id ? { ...exist, qty: product.qty += 1 } : x
+          x.id === product.id ? { ...exist, qty: (product.qty += 1) } : x
         )
       );
       update(product);
     } else {
-      setCartProduct([...cartProduct, { ...product, qty: product.qty = 1 }]);
+      setCartProduct([...cartProduct, { ...product, qty: (product.qty = 1) }]);
       add(product);
     }
   };
@@ -27,9 +27,10 @@ function ShoppingMain() {
     const exist = cartProduct.find((x) => x.id === product.id);
     if (exist.qty === 1) {
       setCartProduct(cartProduct.filter((x) => x.id !== product.id));
+      remove(product.id);
     } else {
       cartProduct.map((x) =>
-        x.id === product.id ? {...exist, qty: product.qty -= 1} : x
+        x.id === product.id ? { ...exist, qty: (product.qty -= 1) } : x
       );
       update(product);
     }
@@ -46,37 +47,98 @@ function ShoppingMain() {
   const update = async (data) => {
     const onSuccess = await ShoppingApi.updateItem(data);
     if (onSuccess) {
-      alert(data.qty);
       fetchPosts();
     }
   };
 
-    const fetchPosts = async () => {
-      const response = await ShoppingApi.getAllItems();
-      setCartProduct(response.data);
-    };
-    
-    useEffect(() => {
+  const remove = async (id) => {
+    const onSuccess = await ShoppingApi.deleteItem(id);
+    if (onSuccess) {
       fetchPosts();
-    }, []);
+    }
+  };
 
+  const fetchPosts = async () => {
+    const response = await ShoppingApi.getAllItems();
+    setCartProduct(response.data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   return (
     <>
-      <div id="sidebar">
-        <h6 class="font-weight-bold">Your Cart</h6>
-        <ShoppingCart cartItem={cartItem} onAdd={onAdd} onRemove={onRemove} cartProduct={cartProduct} />
+      <nav class="navbar navbar-expand-sm navbar-light bg-white border-bottom">
+        <a class="navbar-brand ml-2 font-weight-bold" href="#">
+          <span id="burgundy">The</span>
+          <span id="orange">Bakeshop</span>
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarColor"
+          aria-controls="navbarColor"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>{" "}
+        </button>
+      </nav>
+      <div class="filter">
+        {" "}
+        <button
+          class="btn btn-default"
+          type="button"
+          data-toggle="collapse"
+          data-target="#mobile-filter"
+          aria-expanded="true"
+          aria-controls="mobile-filter"
+        >
+          Filters<span class="fa fa-filter pl-1"></span>
+        </button>
       </div>
+      <div id="mobile-filter">
+        <p class="pl-sm-0 pl-2">
+          {" "}
+          Home | <b>All Breads</b>
+        </p>
+        <div class="border-bottom pb-2 ml-2">
+          <h4 id="burgundy">Filters</h4>
+        </div>
+        <ShoppingCart
+          cartItem={cartItem}
+          onAdd={onAdd}
+          onRemove={onRemove}
+          cartProduct={cartProduct}
+        />
+      </div>
+      <section id="sidebar">
+        <p>
+          {" "}
+          Home | <b>All Breads</b>
+        </p>
+        <div class="border-bottom pb-2 ml-2">
+          <h4 id="burgundy">Your Cart</h4>
+        </div>
+        <ShoppingCart
+          cartItem={cartItem}
+          onAdd={onAdd}
+          onRemove={onRemove}
+          cartProduct={cartProduct}
+        />
+      </section>
 
-      <div id="products">
-        <div class="container mt-5">
+      <section id="products">
+        <div class="container">
           <div class="row">
             {itemList.map((product) => (
               <StartShopping key={product.id} product={product} onAdd={onAdd} />
             ))}
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
